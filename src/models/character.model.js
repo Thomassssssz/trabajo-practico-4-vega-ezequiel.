b26aa4b795593c69d19e4166775642728f22527b
Tomas Vega <ezequielvegayt@gmail.com>
2025-06-26 09:24:13 -0300

    Escribi el modelo para la base de datos

---
+++ src/models/character.model.js
@@ -0,0 +1,31 @@
+import { DataTypes } from "sequelize";
+import  sequelize  from "../config/database.js";
+
+const Character = sequelize.define("character",{
+    id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    ki:{
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    race:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    gender:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description:{
+        type: DataTypes.STRING
+    },
+});
+
+export default Character; 
