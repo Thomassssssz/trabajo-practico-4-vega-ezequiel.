1b4e996de2e1892c8b65b38865275cc806d22ee8
Tomas Vega <ezequielvegayt@gmail.com>
2025-06-26 09:37:53 -0300

    para levantar el servidor

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-import express from 'express'
+import express from 'express';
+import dotenv from 'dotenv';
+import CharacterRoutes from './src/routes/character.routes.js';
+import { startDB } from  './src/config/database.js';
 
-const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+dotenv.config(); 
 
-app.listen(3000)
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+
+app.use(express.json()); 
+app.use("/api", CharacterRoutes);
+
+
+app.use((req, res)=>{
+  res.status(404).json({errorMessage: "Direccion not HostNotFoundError."});
+});
+
+startDB().then(()=>{
+    console.log("Servidor corriendo: ");
+  })
+
+  app.listen(PORT, async () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+  await startDB(); // inicia la conexión con la base de datos
+});
